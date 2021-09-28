7f5f302a20073f711e6ef2ed063af0de1b5460ec
Scott Hutley <scotthutley1@comcast.net>
2021-09-28 15:07:23 -0700

    Create Parcel class, link with Shipping.Index
    Co-authored-by: Charles Weber <charlestweber@gmail.com>

---
+++ Controllers/ShippingController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+using System.Collections.Generic;
+
+namespace Parcels.Controllers
+{
+  public class ShippingController : Controller
+  {
+    [HttpPost("/Shipping/Index")]
+    public ActionResult Index(int width, int length, int height, int weight)
+    {
+      Parcel parcelToSend = new Parcel(width, length, height, weight);
+      return View(parcelToSend);
+    }
+  }
+}
---
+++ Models/Parcel.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace Parcels.Models
+{
+  public class Parcel
+  {
+    public int Width { get; set; }
+    public int Length { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+
+    private static List<Parcel> _instances = new List<Parcel> {};
+
+    public Parcel(int width, int length, int height, int weight)
+    {
+      Width = width;
+      Length = length;
+      Height = height;
+      Weight = weight;
+      _instances.Add(this);
+    }
+
+    public static List<Parcel> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public int FindVolume()
+    {
+      return (Width * Length * Height);
+    }
+    public int CostToShip(int Volume)
+    {
+      return (Volume * Weight);
+    }
+  }
+}
